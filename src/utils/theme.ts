110607d88fe53ad2d3053ffafbaea884034d3e9d
WrathOfZombies <bhargav.krishna@outlook.com>
2023-12-29 14:12:58 -0800

    feat: using urbanhive fonts

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,22 @@
 "use client";
 
-import { createTheme } from "@mantine/core";
+import { createTheme, rem } from "@mantine/core";
+import { Outfit, Space_Grotesk } from "next/font/google";
 
-export default createTheme({});
+const headingFont = Space_Grotesk({ subsets: ["latin"] });
+const bodyFont = Outfit({ subsets: ["latin"] });
+
+export default createTheme({
+  headings: {
+    fontWeight: "400",
+    fontFamily: headingFont.style.fontFamily,
+  },
+  fontFamily: bodyFont.style.fontFamily,
+  fontSizes: {
+    xs: rem(10),
+    sm: rem(11),
+    md: rem(14),
+    lg: rem(16),
+    xl: rem(20),
+  },
+});
