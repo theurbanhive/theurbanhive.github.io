95b8b02884ff1e6bab8cef96f4a5682331914d09
WrathOfZombies <bhargav.krishna@outlook.com>
2023-12-29 15:48:39 -0800

    feat: decided to use a single page scroll layout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,83 +1,17 @@
-"use client";
-
-import {
-  Box,
-  Burger,
-  Button,
-  Divider,
-  Drawer,
-  Group,
-  ScrollArea,
-  rem,
-} from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { Box, Group, rem } from "@mantine/core";
+import Link from "next/link";
 import styles from "./header.module.css";
 
 export default () => {
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
-    useDisclosure(false);
-
   return (
-    <Box pb={120}>
+    <Box pb={rem(200)}>
       <header className={styles.header}>
-        <Group justify="space-between" h="100%">
-          <Group h="100%" gap={0} visibleFrom="sm">
-            <a href="#" className={styles.link}>
-              Home
-            </a>
-
-            <a href="#" className={styles.link}>
-              Learn
-            </a>
-            <a href="#" className={styles.link}>
-              Academy
-            </a>
-          </Group>
-
-          <Group visibleFrom="sm">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
-          </Group>
-
-          <Burger
-            opened={drawerOpened}
-            onClick={toggleDrawer}
-            hiddenFrom="sm"
-          />
+        <Group justify="center" h="100%">
+          <Link href="/" className={styles.brand}>
+            Urbanhive
+          </Link>
         </Group>
       </header>
-
-      <Drawer
-        opened={drawerOpened}
-        onClose={closeDrawer}
-        size="100%"
-        padding="md"
-        title="Navigation"
-        hiddenFrom="sm"
-        zIndex={1000000}
-      >
-        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
-          <Divider my="sm" />
-
-          <a href="#" className={styles.link}>
-            Home
-          </a>
-
-          <a href="#" className={styles.link}>
-            Learn
-          </a>
-          <a href="#" className={styles.link}>
-            Academy
-          </a>
-
-          <Divider my="sm" />
-
-          <Group justify="center" grow pb="xl" px="md">
-            <Button variant="default">Log in</Button>
-            <Button>Sign up</Button>
-          </Group>
-        </ScrollArea>
-      </Drawer>
     </Box>
   );
 };
