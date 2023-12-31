2529399830ca6c98bc86af16e44af50e5549ae55
WrathOfZombies <bhargav.krishna@outlook.com>
2023-12-30 23:33:46 -0800

    feat: metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,29 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="bg-background">
+    <Html lang="en">
       <Head>
-        <meta name="robots" content="noindex" />
         <ColorSchemeScript />
-        <link rel="shortcut icon" href="/favicon.ico" />{" "}
+        <meta name="robots" content="noindex" />
+        <link rel="shortcut icon" href="assets/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="assets/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="assets/favicon-16x16.png"
+        />
+        <link rel="manifest" href="assets/site.webmanifest" />
       </Head>
 
       <body>
